6650b8f39355b8b392ca9f80c1919a0d17329317
fozcollu <fe.ozcollu@gmail.com>
2022-06-21 10:37:42 +0300

    test added - add unknown amount numbers

--- calculator_test.go
+++ calculator_test.go
@@ -2,7 +2,7 @@ package calculator
 
 import "testing"
 
-func TestCalculator_WithUpTo2Numbers(t *testing.T) {
+func TestAdd_WithUpTo2Numbers(t *testing.T) {
 	testModels := []struct {
 		input    string
 		expected int
@@ -19,5 +19,23 @@ func TestCalculator_WithUpTo2Numbers(t *testing.T) {
 			t.Fatalf("case %d - Expected: %d, Actual: %d", i+1, testModel.expected, actual)
 		}
 	}
+}
 
+func TestAdd_WithUnknownAmountNumbers(t *testing.T) {
+	testModels := []struct {
+		input    string
+		expected int
+	}{
+		{input: "1,2,3", expected: 6},
+		{input: "3,4,6,8,9", expected: 30},
+		{input: "2", expected: 2},
+	}
+
+	stringCalculator := New()
+
+	for i, testModel := range testModels {
+		if actual, _ := stringCalculator.Add(testModel.input); testModel.expected != actual {
+			t.Fatalf("case %d - Expected: %d, Actual: %d", i+1, testModel.expected, actual)
+		}
+	}
 }
